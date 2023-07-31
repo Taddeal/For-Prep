6d6cc512125032ad5303b2457d0e5f5d64824992
Taddeal.Moges <Taddeal.Moges@partner.safaricom.et>
2023-07-31 14:28:28 -0300

    first

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n,num,r,sum=0;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the number");
+        n=s.nextInt();
+        num=n;
+        while(num>9){
+
+            while (num>0){
+
+                r=num%10;
+                sum+=r;
+                num/=10;
+            }
+            num=sum;
+            sum=0;
+        }
+        if (num==1){
+            System.out.println("Majic number");
+        }
+        else {
+            System.out.println("Not Majic Number");
+        }
+
+    }
+}
